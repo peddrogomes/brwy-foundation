de38c6a62666c7ad54a3301b3b870cb8d7b89c4b
Peddrogomes <peddrogomes01@gmail.com>
2025-07-30 22:46:49 -0300

    feat (bucket): add bucket raw on data-project

--- bucket.tf
+++ bucket.tf
@@ -1,7 +1,16 @@
 
 resource "google_storage_bucket" "lnd" {
   project = var.project
-  name = "${var.data-project}-lnd"
+  name = "${var.project}-lnd"
+  force_destroy = false
+  uniform_bucket_level_access = true
+  location = var.region
+  labels = local.labels
+}
+
+resource "google_storage_bucket" "raw" {
+  project = var.data-project
+  name = "${var.project}-raw"
   force_destroy = false
   uniform_bucket_level_access = true
   location = var.region
