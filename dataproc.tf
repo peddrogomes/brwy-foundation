6a5fbe6852aa3c80e03cebbf528d7ee4bdd8ac55
Pedro Gomes <pedro.agomes@g.globo>
2025-08-01 18:33:56 -0300

    fix (dataproc): implementing move dataproc files

--- dataproc.tf
+++ dataproc.tf
@@ -13,63 +13,14 @@ resource "google_storage_bucket" "dataproc-bucket" {
   labels = local.labels
 }
 
-# resource "google_project_iam_member" "dataproc_worker" {
-#   project = var.project
-#   role    = "roles/dataproc.worker"
-#   member  = "serviceAccount:${google_service_account.dataproc-svc.email}"
-# }
+resource "google_storage_bucket_object" "dataproc-code" {
+  for_each = fileset("../scr/dataproc", "**/*")
 
-# resource "google_project_iam_member" "dataproc_storage_admin" {
-#   project = var.project
-#   role    = "roles/storage.admin"
-#   member  = "serviceAccount:${google_service_account.dataproc-svc.email}"
-# }
+  name = "dataproc/${each.value}"
+  bucket = google_storage_bucket.dataproc-bucket.name
+  source = "../scr/dataproc/${each.value}"
+}
 
-# resource "google_project_iam_member" "dataproc_bigquery_admin" {
-#   project = var.project
-#   role    = "roles/bigquery.admin"
-#   member  = "serviceAccount:${google_service_account.dataproc-svc.email}"
-# }
-
-# resource "google_dataproc_cluster" "mycluster" {
-  
-#   name = "brwy-dataproc"
-#   region = var.region
-
-#   cluster_config {
-#     staging_bucket = google_storage_bucket.dataproc-bucket.name
-
-#     master_config {
-#       num_instances = 1
-#       machine_type  = "e2-standard-2"
-#       disk_config {
-#         boot_disk_type    = "pd-standard"
-#         boot_disk_size_gb = 100
-#       }
-#     }
-
-#     worker_config {
-#       num_instances = 2
-#       machine_type  = "e2-standard-2"
-#       disk_config {
-#         boot_disk_type    = "pd-standard"
-#         boot_disk_size_gb = 100
-#         num_local_ssds    = 0
-#       }
-#     }
-
-#     software_config {
-#       image_version = "2.0.66-debian10"
-#     }
-
-#     gce_cluster_config {
-#       zone = "${var.region}-b"
-#       subnetwork             = var.subnet_name
-#       service_account        = google_service_account.dataproc-svc.email
-#       service_account_scopes = ["cloud-platform"]
-#     }
-#   }
-# }
 
 resource "google_dataproc_workflow_template" "brwy_pipeline" {
   name     = "brwy-pipeline-template"
