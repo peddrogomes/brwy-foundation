e897c1019107569e2e5a32f3e2d68e8082d8e34a
Pedro Gomes <pedro.agomes@g.globo>
2025-08-03 13:02:06 -0300

    fix (iam): fix bigquery acess with SA

--- iam.tf
+++ iam.tf
@@ -8,19 +8,6 @@ resource "google_storage_bucket_iam_member" "silver_editor" {
 # BigQuery permissions for Dataproc service account
 resource "google_bigquery_dataset_iam_member" "breweries_foundation_editor" {
   dataset_id = google_bigquery_dataset.breweries_foundation.dataset_id
-  role       = "roles/bigquery.dataEditor"
+  role       = "roles/bigquery.admin"
   member     = "serviceAccount:966844133549-compute@developer.gserviceaccount.com"
-}
-
-resource "google_bigquery_dataset_iam_member" "breweries_foundation_user" {
-  dataset_id = google_bigquery_dataset.breweries_foundation.dataset_id
-  role       = "roles/bigquery.user"
-  member     = "serviceAccount:966844133549-compute@developer.gserviceaccount.com"
-}
-
-# Project-level BigQuery permissions for the service account
-resource "google_project_iam_member" "bigquery_job_user" {
-  project = var.project
-  role    = "roles/bigquery.jobUser"
-  member  = "serviceAccount:966844133549-compute@developer.gserviceaccount.com"
 }
